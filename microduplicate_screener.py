16207d83586be2fbf73d58edae31c8789aa4bf78
builab <huy.bui@mcgill.ca>
2022-02-07 00:05:47 -0500

    Update microduplicate_screener.py

--- microduplicate_screener.py
+++ microduplicate_screener.py
@@ -8,7 +8,10 @@ Micrograph duplicate screener
 @author: kbui2
 """
 
-
+import numpy as np
+import starfile
+import argparse
+import mrcfile
 
 
 if __name__=='__main__':
@@ -16,9 +19,10 @@ if __name__=='__main__':
 	print('WARNING: Only compatible with Relion 3.1 star file & McGill pattern of holes')
 	
 	parser = argparse.ArgumentParser(description='')
-	parser.add_argument('--imdir', help='Input image folder',required=True)
-  parser.add_argument('--outdir', help='Output folder (Temp)',required=True)
+	parser.add_argument('--i', help='Input star file (from MotionCorr or CtfFind)',required=True)
+ 	parser.add_argument('--outdir', help='Output folder (Temp)',required=True)
 	parser.add_argument('--bin', help='Binning',required=False,default=8)
+	parser.add_argument('--opticsless', help='With or without opticsgroup. Value 1 or 0',required=False,default="1")
 	parser.add_argument('--scanrange', help='Range for scanning',required=False,default=48)
 	parser.add_argument('--threshold', help='CCC threshold to set as duplicate',required=False,default=0.25)
 
@@ -32,3 +36,21 @@ if __name__=='__main__':
  	binning = int(args.bin)
 	screenrange = int(args.scanrange)
 	threshold = float(args.threshold)
+	
+	
+	
+	stardict = starfile.read(args.i)
+	if (args.opticsless == 1):
+		df = stardict
+	else:
+		df = stardict['micrographs']
+		
+	dfmicrograph = df["rlnMicrographName"]
+	nosubtomo = len(dfmicrograph)
+	
+	# loop through micrograph
+	for i in len(dfmicrograph):
+		
+
+
+	
